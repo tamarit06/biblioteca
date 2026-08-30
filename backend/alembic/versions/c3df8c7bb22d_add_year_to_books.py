dc383f32e0508f6dd119a6b686406d1855e6b393
Tamarit <tamarittejas@gmail.com>
2026-08-29 23:46:13 -0400

    fix

--- backend/alembic/versions/c3df8c7bb22d_add_year_to_books.py
+++ backend/alembic/versions/c3df8c7bb22d_add_year_to_books.py
@@ -1,29 +1,30 @@
-"""add year to books
-
-Revision ID: c3df8c7bb22d
-Revises: e8c6c617ab86
-Create Date: 2026-08-29 19:09:40.839484
-
-"""
-from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
-revision: str = 'c3df8c7bb22d'
-down_revision: Union[str, Sequence[str], None] = 'e8c6c617ab86'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
-
-
 def upgrade() -> None:
-    op.add_column(
-        "books",
-        sa.Column("year", sa.Integer(), nullable=False, server_default="0")
-    )
+    bind = op.get_bind()
+    inspector = sa.inspect(bind)
+
+    columns = [column["name"] for column in inspector.get_columns("books")]
+
+    if "year" not in columns:
+        op.add_column(
+            "books",
+            sa.Column(
+                "year",
+                sa.Integer(),
+                nullable=False,
+                server_default="0"
+            )
+        )
 
 
 def downgrade() -> None:
-    op.drop_column("books", "year")
+    bind = op.get_bind()
+    inspector = sa.inspect(bind)
+
+    columns = [column["name"] for column in inspector.get_columns("books")]
+
+    if "year" in columns:
+        op.drop_column("books", "year")
