17e6653f4a8613d5f86f4fd2c03272471d99a8a5
Tamarit <tamarittejas@gmail.com>
2026-08-29 21:34:47 -0400

    fix: allow production frontend in cors"

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://biblioteca-fronted.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
